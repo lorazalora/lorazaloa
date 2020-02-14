fba60ff9b59da1224933f4410e93d78d7959e7ff
lorazalora <55939920+lorazalora@users.noreply.github.com>
2020-02-14 09:47:28 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -135,46 +135,38 @@ async def amireallyalive(on):
                      "====================== "
 
                                          "")
-
-
-@register(events.NewMessage(pattern="^.hack$",outgoing=True))
-
-async def amireallyevent(event):
-
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 2
-
-    animation_ttl = range(0, 11)
-
-    input_str = event.pattern_match.group(1)
-
-    if input_str == "hack":
-
-        await event.edit(input_str)
-
-        animation_chars = [
-        
-            "`Connecting To Hacked Private Server...`",
-            "`Target Selected.`",
-            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
-            "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 9999$ To @Lorazalora or a Plate of Samosas To Remove This Hack`"
-        ]
-
-        for i in animation_ttl:
-
-            await asyncio.sleep(animation_interval)
-
-            await event.edit(animation_chars[i % 11])
+await on.edit("`"
+                     f"Connecting To Hacked Private Server..."
+                                         "")
+await on.edit("`"
+                     f"Target Selected."
+                                         "")
+                                         await on.edit("`"
+                     f"Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+                                         "")
+                                         await on.edit("`"
+                     f"Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+                                         "")
+                                         await on.edit("`"
+                     f"Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒."
+                                         "")
+                                         await on.edit("`"
+                     f"Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+                                         "")await on.edit("`"
+                     f"Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒"
+                                         "")
+                                         await on.edit("`"
+                     f"Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒."
+                                         "")
+await on.edit("`"
+                     f"Hacking... 84%\n█████████████████████▒▒▒▒"
+                                         "")
+await on.edit("`"
+                     f"Hacking... 100%\n█████████HACKED███████████"
+                                         "")
+                                         await on.edit("`"
+                     f"Targeted Account Hacked...\n\nPay 9999$ To @Lorazalora or a Plate of Samosas To Remove This Hack"
+                                         "")
 
 
 @register(outgoing=True, pattern="^.aliveu")
@@ -215,13 +207,3 @@ CMD_HELP.update({
     \n\n.resetalive\
     \nUsage: Resets the user to default."
 })
-CMD_HELP.update({
-"hack":
-    ".hack\
-    \nUsage: Type .on to see wether your bot is working or not.\
-    \n\n.aliveu <text>\
-    \nUsage: Changes the 'user' in alive to the text you want.\
-    \n\n.resetalive\
-    \nUsage: Resets the user to default."
-})
-
